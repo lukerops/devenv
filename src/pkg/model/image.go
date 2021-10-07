2c701a5f4d17c28b137209d896c32aab8286bb79
Lucas Vieira <lucasscvvieira@gmail.com>
2021-10-06 23:24:50 -0300

    feat: add images model to filter results

--- src/pkg/model/image.go
+++ src/pkg/model/image.go
@@ -10,3 +10,54 @@ type Image struct {
 	Size       int64
 	CreatedAt  time.Time
 }
+
+type Images []*Image
+
+func (imgs Images) FilterByNames(names []string) Images {
+	var filtered Images
+
+	for _, name := range names {
+		filtered = append(filtered, imgs.FilterByName(name)...)
+	}
+
+	return filtered
+}
+
+func (imgs Images) FilterByName(name string) Images {
+	var filtered Images
+
+	for _, image := range imgs {
+		for _, imageName := range image.Names {
+			if imageName == name {
+				filtered = append(filtered, image)
+				break
+			}
+		}
+	}
+
+	return filtered
+}
+
+func (imgs Images) FilterByLabels(labels map[string]string) Images {
+	var filtered Images
+
+	for key, value := range labels {
+		filtered = append(filtered, imgs.FilterByLabel(key, value)...)
+	}
+
+	return filtered
+}
+
+func (imgs Images) FilterByLabel(key, value string) Images {
+	var filtered Images
+
+	for _, image := range imgs {
+		if label, ok := image.Labels[key]; ok {
+			if label == value {
+				filtered = append(filtered, image)
+			}
+		}
+	}
+
+	return filtered
+}
